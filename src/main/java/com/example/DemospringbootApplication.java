a32971e4d3c2059498a91754f56c4006207221e0
BHARGAVI <bhargavi07.b@gmail.com>
2025-10-18 02:54:25 +0530

    added files

--- src/main/java/com/example/DemospringbootApplication.java
+++ src/main/java/com/example/DemospringbootApplication.java
@@ -2,13 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemospringbootApplication extends SpringBootServletInitializer{
+public class DemoSpringbootApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemospringbootApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoSpringbootApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootApplication.class, args);
+    }
 }
