255014666166f5d20c3e1c28aa72620e7c6243fa
BHARGAVI <bhargavi07.b@gmail.com>
2025-10-18 02:26:43 +0530

    added files

---
+++ src/main/java/com/example/ClientController.java
@@ -0,0 +1,33 @@
+package com.example;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ClientController 
+{
+	@GetMapping("/")
+   public String home()
+   {
+	return "CI/CD and Cloud Devops EXAM PRACTICE ";
+	
+   }
+	
+	
+	 @GetMapping("/demo")
+	   public String demo()
+	   {
+		   return "Jenkins BACKEND PRACTICE ";
+	   }
+	   
+   @GetMapping("/test")
+   public String test()
+   {
+	   return "Deploy springboot APLLICATION  using Jenkins";
+   }
+   
+}
